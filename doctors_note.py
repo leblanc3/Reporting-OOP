638858eae9da575b520f3da56f18dec951b0d7a8
leblanc3 <renierdelacruz666@gmail.com>
2025-06-05 23:52:02 +0800

    HALLELUYAH

---
+++ doctors_note.py
@@ -0,0 +1,55 @@
+from tkinter import *
+import random
+
+main = Tk()
+main.title("Doctor's Note")
+main.geometry("1280x720")
+
+own_image = PhotoImage(file="C:/Users/ThinkPad/Downloads/jesus.png")
+
+def change_color():
+    color = "#{:06x}".format(random.randint(0, 0xFFFFFF))
+    first_line.config(fg=color)
+    main.after(200, change_color)
+
+font_families = ["Arial", "Courier", "Helvetica", "Times", "Comic Sans", "MS", "Verdana", "Georgia"]
+
+def change_font():
+    random_font = random.choice(font_families)
+    size = random.randint(12, 24)
+    style = random.choice(["normal", "bold", "italic"])
+    second_line.config(font=(random_font, size, style))
+    main.after(1000, change_font)
+
+def lower_lifespan():
+    global months_to_live
+    months_to_live -= 1
+
+    if months_to_live <= 0:
+        first_line.config(text="Your suffering is no longer.")
+        second_line.destroy()
+        upper_line.destroy()
+        Label(main, image=own_image).pack(expand=True)
+    else:
+        second_line.config(text="You have " + str(months_to_live) + " months left to live.")
+
+types_of_cancer = ["Lung", "Pancreatic", "Breast", "Skin", "Prostate"]
+
+type_of_cancer = types_of_cancer[random.randint(0, 4)]
+months_to_live = random.randint(1, 12)
+
+upper_line = Label(main, text="You have:", font=("Arial", 35))
+upper_line.pack(expand=True)
+
+first_line = Label(main, text=type_of_cancer + " Cancer", font=("Arial", 78))
+first_line.pack(expand=True)
+
+second_line = Label(main, text="You have " + str(months_to_live) + " months left to live.")
+second_line.pack(expand=True)
+
+Button(main, text="Click to slowly end your suffering", command=lower_lifespan).pack(expand=True)
+
+change_color()
+change_font()
+
+main.mainloop()
